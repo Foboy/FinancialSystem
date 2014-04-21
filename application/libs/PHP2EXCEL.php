509c747626ef2d88fdd973f128d0ce44b0829614
kbtyc <kbtyc@163.com>
2014-04-21 21:56:44 +0800

    no message

--- application/libs/PHP2EXCEL.php
+++ application/libs/PHP2EXCEL.php
@@ -76,7 +76,7 @@ class PHP2EXCEL {
 // 		header ( "Content-Disposition: attachment; filename=\"$fileName\"" );
 // 		header ( 'Cache-Control: max-age=0' );
 		
-header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=utf-8');
 		header ( "Content-Disposition: attachment; filename=\"$fileName\"" );
 header('Cache-Control: max-age=0');
 // If you're serving to IE 9, then the following may be needed
@@ -87,7 +87,6 @@ header ('Expires: Mon, 26 Jul 1997 05:00:00 GMT'); // Date in the past
 header ('Last-Modified: '.gmdate('D, d M Y H:i:s').' GMT'); // always modified
 header ('Cache-Control: cache, must-revalidate'); // HTTP/1.1
 header ('Pragma: public'); // HTTP/1.0
-header('Content-Type:text/html;charset=utf-8');
 
 		
 		$objWriter = PHPExcel_IOFactory::createWriter ( $objPHPExcel, 'Excel2007' );
